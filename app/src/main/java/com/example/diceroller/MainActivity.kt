c85b62b4f4a998b7be8c9fa60c4f77c2ff6d6cba
Pankaj Singh Kanyal <pankrj123@gmail.com>
2022-02-06 17:31:02 +0530

    Completed Dice roller add

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,12 +12,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
+        val diceImage:ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(R.drawable.dice_1);
+        // This above 3 lines will set the image as dice 1 image.
 
+        var count=1
         //This is the lambda function.
         rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Roled", Toast.LENGTH_SHORT)
+            val toast = Toast.makeText(this, "Dice Roled ${count} times", Toast.LENGTH_SHORT)
             toast.show()
             rollDice()
+            count += 1
         }
     }
 
@@ -31,13 +36,20 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
 
-        val dice2 = Dice( 6)
-        val result2 = dice2.roll()
-        val resultSecondTextView: TextView = findViewById(R.id.textView2)
-        resultSecondTextView.text = result2.toString()
+        val diceImage:ImageView = findViewById(R.id.imageView)
+
+
+        val drawableResource = when(diceRoll)
+        {
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else ->R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource);
         //This is demo comment
     }
 }
